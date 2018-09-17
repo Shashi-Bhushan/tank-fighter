9d87b1fd2cf949a8e8477b8044176da0ecdd88ec
Shashi Bhushan <sbhushan1@outlook.com>
2018-09-17 12:02:54 +0530

    Activator Updated

--- in.shabhushan.tankfighter.game/src/main/java/in/shabhushan/tankfighter/game/activator/TankFighterActivator.java
+++ in.shabhushan.tankfighter.game/src/main/java/in/shabhushan/tankfighter/game/activator/TankFighterActivator.java
@@ -5,31 +5,39 @@ import in.shabhushan.tankfighter.game.service.internal.CircleGameServiceImpl;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-import java.util.Dictionary;
 import java.util.Hashtable;
-import java.util.Properties;
 
 import static java.lang.System.out;
 
 /**
- * Extension of default
+ * Extension of default OSGi bundle Activator
  *
  * @author Shashi Bhushan
  * @date 17/9/18
  */
 public class TankFighterActivator implements BundleActivator {
 
+    /**
+     * Called when OSGi framework starts our bundle
+     * @param bundleContext
+     * @throws Exception
+     */
     @Override
     public void start(BundleContext bundleContext) throws Exception {
         out.println("Starting " + this.getClass().getName() + "#start");
 
         Hashtable properties = new Hashtable();
         properties.put("osgi.command.scope", "tank");
-        properties.put("osgi.command.function", "showCircle");
+        properties.put("osgi.command.function", new String[]{"startGame", "stopGame"});
 
         bundleContext.registerService(CircleGameService.class.getName(), new CircleGameServiceImpl(), properties);
     }
 
+    /**
+     * Called when OSGi stops our bundle
+     * @param bundleContext
+     * @throws Exception
+     */
     @Override
     public void stop(BundleContext bundleContext) throws Exception {
         out.println("Stopping " + this.getClass().getName() + "#start");
