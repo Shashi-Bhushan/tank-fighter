3b9f1b5200968807f1e7b54053333e707db91e8b
Shashi Bhushan <shabhushan@deloitte.com>
2018-09-21 19:03:23 +0530

    Log4J to be implemented

--- core/src/main/java/in/shabhushan/tankfighter/game/activator/TankFighterActivator.java
+++ core/src/main/java/in/shabhushan/tankfighter/game/activator/TankFighterActivator.java
@@ -1,9 +1,12 @@
 package in.shabhushan.tankfighter.game.activator;
 
+import in.shabhushan.tankfighter.game.model.Tank;
 import in.shabhushan.tankfighter.game.service.TankGameService;
 import in.shabhushan.tankfighter.game.service.internal.TankGameServiceImpl;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Hashtable;
 
@@ -17,6 +20,8 @@ import static java.lang.System.out;
  */
 public class TankFighterActivator implements BundleActivator {
 
+    private static final Logger LOG = LoggerFactory.getLogger(TankFighterActivator.class);
+
     /**
      * Called when OSGi framework starts our bundle
      * @param bundleContext
@@ -25,6 +30,7 @@ public class TankFighterActivator implements BundleActivator {
     @Override
     public void start(BundleContext bundleContext) throws Exception {
         out.println("Starting " + this.getClass().getName() + "#start");
+        LOG.info("Starting {} #start", this.getClass().getName());
 
         Hashtable tankProperties = new Hashtable();
         tankProperties.put("osgi.command.scope", "tank");
@@ -40,7 +46,7 @@ public class TankFighterActivator implements BundleActivator {
      */
     @Override
     public void stop(BundleContext bundleContext) throws Exception {
-        out.println("Stopping " + this.getClass().getName() + "#start");
-
+        out.println("Stopping " + this.getClass().getName() + "#stop");
+        LOG.info("Stopping {} #stop", this.getClass().getName());
     }
 }
